b73c81afeacd1d9f67f62c93d326bd604bb2a254
LSM <dltmdals6718@naver.com>
2023-11-16 17:28:29 +0900

    app init

---
+++ app/src/main/java/com/example/sw03_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sw03_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
