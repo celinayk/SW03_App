85317e010f9b11fe873195503baf9cbfea5f6a62
ysbin0505 <chfhddl515@gmail.com>
2023-11-19 00:12:34 +0900

    기본 화면

---
+++ app/src/main/java/com/example/sw03_app/KakaoUserInfo.java
@@ -0,0 +1,11 @@
+package com.example.sw03_app;
+
+public class KakaoUserInfo {
+    private String kakaoUserName;
+
+    private String kakaoUserProfile;
+    public KakaoUserInfo(String kakaoUserName, String kakaoUserProfile){
+        this.kakaoUserName = kakaoUserName;
+        this.kakaoUserProfile = kakaoUserProfile;
+    }
+}
--- app/src/main/java/com/example/sw03_app/MainActivity.java
+++ app/src/main/java/com/example/sw03_app/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.sw03_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
