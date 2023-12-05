8a42e6bd23d305e4a1aa18e3d862c9344534e906
praisebak <praisebak@naver.com>
2023-12-05 10:52:10 +0900

    NoReservation gps 기능 추가

--- app/src/main/java/com/example/sw03_app/NoReservation.java
+++ app/src/main/java/com/example/sw03_app/NoReservation.java
@@ -1,17 +1,25 @@
 package com.example.sw03_app;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.core.app.ActivityCompat;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -24,6 +32,7 @@ import com.kakao.sdk.user.model.User;
 import kotlin.Unit;
 import kotlin.jvm.functions.Function2;
 import okhttp3.OkHttpClient;
+import org.jetbrains.annotations.NotNull;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -39,10 +48,148 @@ public class NoReservation extends AppCompatActivity {
     private long snsId;
     private long seatId;
 
+
+    private static final long ONE_HOUR_IN_MILLIS = 60 * 60 * 1000;
+
+    //6초
+    private static final long ONE_MIN_IN_MILLIS = 6 * 1000;
+
+    private static final int PERMISSION_REQUEST_CODE = 1001;
+    private UserLocationManager userLocationManager;
+    private boolean prevChecked = false;
+
+    private ApiService apiService;
+
+    public static Location getLibraryLocation() {
+        Location libraryLocation = new Location("library");
+        libraryLocation.setLatitude(35.246440); // Library's latitude
+        libraryLocation.setLongitude(128.690815); // Library's longitude
+        return libraryLocation;
+    }
+
+
+    private void startCount(){
+        userLocationManager = new UserLocationManager(NoReservation.this, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                // 위치가 변경될 때마다 실행되는 부분
+            }
+        });
+
+        System.out.println(userLocationManager);
+        System.out.println(userLocationManager.getLastKnownLocation(NoReservation.this));
+
+        new CountDownTimer(ONE_HOUR_IN_MILLIS, ONE_MIN_IN_MILLIS) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+
+                Location libraryLocation = getLibraryLocation();
+                LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                Location currentLocation = userLocationManager.getLastKnownLocation(NoReservation.this);
+
+                System.out.println(currentLocation.getLatitude() + "," + currentLocation.getLongitude());
+                if (currentLocation != null) {
+                    float distance = currentLocation.distanceTo(libraryLocation);
+                    //오차범위 포함 + 10미터
+                    if (distance > 110) {
+                        if(prevChecked){
+                            Toast.makeText(NoReservation.this, "도서관에서 100m 멀어졌어요! 좌석이 취소됩니다", Toast.LENGTH_SHORT).show();
+                            cancelRequest();
+                            return;
+                        }else{
+                            AlertDialog alertDialog = getAlertDialog("도서관에서 100m 멀어졌어요! 10분 안에 복귀해주세요.", "확인");
+                            alertDialog.show();
+                            cancelRequest();
+                        }
+                        prevChecked = true;
+                    } else {
+                        Toast.makeText(NoReservation.this, "디버깅 용 도서관에서 100미터 이내입니다.", Toast.LENGTH_SHORT).show();
+                        prevChecked = false;
+                    }
+                } else {
+                    if(prevChecked == false){
+                        Toast.makeText(NoReservation.this, "위치 정보를 확인할 수 없습니다 위치 정보를 확인해주세요", Toast.LENGTH_SHORT).show();
+                        prevChecked = true;
+                    }else{
+                        Toast.makeText(NoReservation.this, "위치 정보를 확인할 수 없습니다 좌석이 취소됩니다", Toast.LENGTH_SHORT).show();
+                        cancelRequest();
+                        return;
+                    }
+                }
+            }
+
+
+            @Override
+            public void onFinish() {
+                cancelRequest();
+            }
+
+        }.start();
+
+    }
+
+
+    @NotNull
+    private AlertDialog getAlertDialog(String message, String check) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(NoReservation.this);
+        alertDialogBuilder.setMessage(message)
+                .setPositiveButton(check, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        return alertDialog;
+    }
+
+
+    private void cancelRequest() {
+        Toast.makeText(NoReservation.this, "장작", Toast.LENGTH_SHORT).show();
+
+        // DELETE API 호출
+        String seatUsernameToDelete = "praisebak"; // 삭제할 좌석의 ID
+        Call<Void> call = apiService.deleteSeat(seatUsernameToDelete);
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    // 성공적으로 삭제됨
+
+                } else {
+                    //클라이언트에서 서버로 요청을 보내는걸 실패했다? 그럼 어떡해요
+                    // 요청 실패 처리
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                t.printStackTrace();
+            }
+
+        });
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.no_reservation);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:8080")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        apiService = retrofit.create(ApiService.class);
+
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_CODE);
+        }else{
+
+        }
+
 
         Button reservationBtn; // 예약 버튼
         FloatingActionButton out_button, redo_button; // x 버튼(홈으로), < 버튼(뒤로)
@@ -58,12 +205,15 @@ public class NoReservation extends AppCompatActivity {
                 UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
                     @Override
                     public Unit invoke(User user, Throwable throwable) {
-                        snsId = user.getId();
+                        System.out.println("냠냠");
 
+                        snsId = user.getId();
                         // 서버에 예약 요청 보내는 부분
-                        sendReservationRequest();
-                        saveReservedSeatToSharedPreferences();
-                        exitCurrentPage();
+                        startCount();
+
+//                        sendReservationRequest();
+//                        saveReservedSeatToSharedPreferences();
+//                        exitCurrentPage();
 
                         return null;
                     }
